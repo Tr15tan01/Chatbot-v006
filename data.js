6edf1ae55619b89252530240a5292783c28e8092
titevar <59051464+titevar@users.noreply.github.com>
2021-11-06 23:09:39 +0400

    added some common phreses

--- data.js
+++ data.js
@@ -1,14 +1,26 @@
 const datObject = {
-    hi: 'hello',
-    bye: 'bye bye',
-    // ' ': 'say something',
-    arr: 'noce',
+    'hi': 'hello',
+    'bye': 'bye bye',
     'how are': 'I am good, and how are you?',
+    'nice': 'That\'s good',
     'do you know everything': 'I do not know everything',
     'what is your name': 'I have no name yet',
     'google': '<a class="linkStyle" href="https://google.com" target="_blank">google</a>',
     'weather': '<a href="https://www.google.com/search?q=google+local+weather" target="_blank">Ask Google or PRESS Here</a>',
     'what is chatbot': '<a href="https://www.google.com/search?q=what+is+chatbot" target="_blank">See it here</a>',
+    'what is a chatbot': '<a href="https://www.google.com/search?q=what+is+chatbot" target="_blank">See it here</a>',
+    'who are you': 'I am a chatbot',
+    'what are you': 'I am a chatbot',
+    'what\s up': 'Oh, just another wonderful day going',
+    'good morning': 'Good morning to you',
+    'good evening': 'Good evening to you',
+    'good night': 'Good night to you',
+    'tell me something': 'Ok, I think you would like to check this page here...',
+    'do you love me': 'I love you very much',
+    'are you human': 'No, I am a bot, but almost as clever as a human',
+    'where do you live': 'I live on a server',
+    'who\'s your boss': 'You are',
+    'can you help me': 'Ok, I will try'
 };
 
 module.exports = datObject;
